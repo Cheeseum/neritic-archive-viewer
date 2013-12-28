4d684ba2a2c965ba2a0cf1a6adc59db9595aa68d
Andres V <darklink898@gmail.com>
2013-12-27 23:58:07 -0600

    pulling posts from thread 1 for now

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,9 +1,28 @@
-var neriticArchive= angular.module('neriticArchiveViewer', []);
+/* global ub*/
+var neriticArchive = angular.module('neriticArchive', []);
 
-neriticArchive.controller('PostListCtrl', function ($scope) {
-    //TODO: actually retrieve posts from archive
-    $scope.posts = [
-        {'content': 'some random content'}
-    ];
-    $scope.butts = "butts"
+ub.init({
+    baseUrl: 'http://archive.neritic.net/',
+    type: 'jsonp',
+    resourceMap: {
+        'categories': 'categories',
+        'category': 'categories/:p',
+        'forums-by-category': 'categories/:p/forums',
+        'forum': 'forums/:p',
+        'threads-by-forum': 'forums/:p/threads',
+        'thread': 'threads/:p',
+        'posts-by-thread': 'threads/:p/posts',
+        'users': 'users',
+        'user': 'users/:p'
+    }
+});
+
+neriticArchive.controller('PostListCtrl', function ($scope, $http) {
+    $http({
+        'method': 'jsonp',
+        'url': ub.get('posts-by-thread', 1),
+        cache: true
+    }).success(function (data) {
+        $scope.posts = data.content;
+    });
 });
