b7cbcd899a20699d89d10de76ddec23850691f12
Andres V <darklink898@gmail.com>
2013-12-28 10:01:13 -0600

    finally got something working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,22 @@ var navControllers = angular.module('navControllers', [
     'navServices'
 ]);
 
+navControllers.controller('CategoryListCtrl', function ($scope, Category) {
+    $scope.categories = Category.query();
+});
+
+navControllers.controller('ForumListCtrl', function ($scope, Forum) {
+    $scope.forums = Forum.query({category: $scope.category.id});
+});
+
+navControllers.controller('ThreadListCtrl', function ($scope, Thread) {
+    $scope.threads = Thread.query({forum: $scope.forum.id});
+});
+
 navControllers.controller('PostListCtrl', function ($scope, Post) {
-    $scope.posts = Post.query({thread: 1});
+    $scope.posts = Post.query({thread: $scope.thread.id});
+});
+
+navControllers.controller('UserListCtrl', function ($scope, User) {
+    $scope.users = User.query();
 });
--- app/js/services.js
+++ app/js/services.js
@@ -1,20 +1,39 @@
 var navServices = angular.module('navServices', ['ngResource']);
 
-function u (url) {
-    var apiUrl = 'http://archive.neritic.net/';
-    return apiUrl + url;
-}
-
-navServices.factory('Post', function ($resource) {
-    return $resource(u('threads/:thread/posts'), {}, {
-        query: {
+function makeJsonpResource($resource, url, opts) {
+    var defaultOpts = {
             method: 'JSONP',
             cache: true,
-            isArray: true,
             params: {callback: 'JSON_CALLBACK'},
             transformResponse: function (data) {
                 return data.content;
             }
-        }
+        },
+        defaultResource = {
+            get: angular.extend({}, defaultOpts, {isArray: false}),
+            query: angular.extend({},defaultOpts, {isArray: true})
+        },
+        u = function (url) {
+            var apiUrl = 'http://archive.neritic.net/';
+            return apiUrl + url;
+        };
+
+    opts = opts || {};
+    return $resource(u(url), {}, angular.extend(defaultResource, opts));
+}
+
+navServices.factory('Category', function ($resource) {
+    return makeJsonpResource($resource, 'categories/:id');
+});
+
+navServices.factory('Forum', function ($resource) {
+    return makeJsonpResource($resource, 'categories/:category/forums', {
+        get: {url: 'forums/:id'}
+    });
+});
+
+navServices.factory('Thread', function ($resource) {
+    return makeJsonpResource($resource, 'forums/:forum/threads', {
+        get: {url: 'threads/:id'}
     });
 });
